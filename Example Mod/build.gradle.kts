2349dbc18d5d8b8c1c9e1faadd8fcab261c6fda1
SmushyTaco <smushytaco@gmail.com>
2022-08-13 11:42:08 -0700

    Update build.gradle.kts

--- Example Mod/build.gradle.kts
+++ Example Mod/build.gradle.kts
@@ -34,8 +34,6 @@ tasks {
     }
     withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
         kotlinOptions { jvmTarget = javaVersion.toString() }
-        sourceCompatibility = javaVersion.toString()
-        targetCompatibility = javaVersion.toString()
     }
     jar { from("LICENSE") { rename { "${it}_${base.archivesName}" } } }
     processResources {
